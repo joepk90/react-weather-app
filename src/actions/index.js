42f4e251edf1900f02f67975dc588284b6ab4015
Joe Park-Kennaby <jparkkennaby@gmail.com>
2018-03-06 19:52:59 +0000

    add axios dependancy for ajax requests. set up request URL. Now on Redux-Promise in Practise

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,19 @@
+import axios form 'axios';
 const API_KEY = '5f50f4bba4cccc34c51a8a1aeff5b6a1';
+
+const = ROOT_URL 'http://samples.openweathermap.org/data/2.5/forecast?q=London,us&mode=xml&appid=b6907d289e10d714a6e88b30761fae22';
+const = ROOT_URL `http://samples.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+
+export const FETCH_WEATHER = 'FETCH_WEATHER'
+
+export  fucntion fetchWeather(city) {
+
+  const url = `${ROOT_URL}&q=${city},uk`;
+  const request = axois.get(url);
+
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  }
+}
