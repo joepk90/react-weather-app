4cc23221c29b6d93e1aa71944c1340d07dfc8071
Joe Park-Kennaby <jparkkennaby@gmail.com>
2018-03-10 15:48:37 +0000

    finish ajax request and state manipulation. Setup city tables and chart module. Add weather graphs using react-sparklines and add google maps for each city

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -1,6 +1,9 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { fetchWeather } from '../actions/index';
 
-export default class SearchBar extends Component {
+class SearchBar extends Component {
 
   constructor(props) {
     super(props);
@@ -8,17 +11,20 @@ export default class SearchBar extends Component {
     this.state = {term: '' };
 
     this.onInputChange = this.onInputChange.bind(this);
+    this.onFormSubmit = this.onFormSubmit.bind(this);
   }
 
 onInputChange(event) {
-  console.log(event.target.value)
+  // console.log(event.target.value)
   this.setState({ term: event.target.value});
 }
 
 onFormSubmit(event) {
   event.preventDefault();
 
-// We need to go and fetch weather data.
+  // Fetch weather data.
+  this.props.fetchWeather(this.state.term)
+  this.setState({ term: ''});
 
 }
 
@@ -37,3 +43,9 @@ onFormSubmit(event) {
     );
   }
 }
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({ fetchWeather }, dispatch);
+}
+
+export default connect(null, mapDispatchToProps)(SearchBar);
